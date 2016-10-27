1ade50e2683c30e9ebb2713cf5b49db27cb34bca
Shuyang Shi <shuyang790@gmail.com>
2016-10-27 23:52:03 +0800

    Initializing the file server
    The first version of a working file server, under the directory of `./data`
    (under the project directory).

--- routes/multimedia.js
+++ routes/multimedia.js
@@ -1,9 +1,29 @@
 var express = require('express');
+var path = require('path');
+var typecheck = require('../typecheck');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  var filePath = req.query["filepath"];
+
+  if (!typecheck.check(filePath, "string")) {
+    typecheck.report(res);
+    return;
+  }
+
+  if (filePath.startsWith('../')) {
+    res.send(JSON.stringify({
+      "status": "failure",
+      "message": "illegal path"
+    }));
+    return;
+  } 
+
+  console.log({
+    "requesting": filePath,
+    "providing": path.join(__dirname, '../data', filePath)
+  });
+  res.sendFile(path.join(__dirname, '../data', filePath));
 });
 
 module.exports = router;

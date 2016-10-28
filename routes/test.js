77dd39b887ad089ebf36567fa31a56a1fc9e25c1
Shuyang Shi <shuyang790@gmail.com>
2016-10-28 22:29:20 +0800

    Making a simple test page for form submission
    Currently the test page (/api/test) supports:
    
    - Generate a temporary new ID,
    - Register a new user, and
    - User login.

--- routes/test.js
+++ routes/test.js
@@ -2,9 +2,10 @@ var express = require('express');
 var idgenerator = require('../idgenerator');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send(idgenerator.genInt('user').toString());
+router.get('/', async function(req, res, next) {
+  res.render('test', {
+    new_id: await idgenerator.genInt(),
+  });
 });
 
 module.exports = router;

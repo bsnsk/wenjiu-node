ee0c259f56934838534789e32f83052eeb46e648
Shuyang Shi <shuyang790@gmail.com>
2016-12-01 14:36:24 +0800

    Adding new app to post push notification to its platform
    Currently using Yunba as the push notification platform, we need to
    regularly scan the database and send messages to Yunba. Sending is
    implemented using HTTP requests (refer to Yunba document for details).
    
    This is a standalone node.js app (`pushScanner.js`) and should be run by pm2
    (or its alternative). It regularly scans the database to find responses
    to be pushed, and send corresponding information (`request_id`, `response_id`,
    `push_time`) to the request publisher according to his/her userid.
    
    The time interval for scanning is now set to 60 seconds.
    
    Also need `--harmony-async-await` option.
    
    Test:
    
    Tested in terminal.
    ```
    $ node --harmony-async-await pushScanner
    { name: 'scheduleScanner:',
      time: 2016-12-01T06:47:00.045Z,
      'currentTime(ms)': 1480574820038 }
    [ BinaryRow {
        userid: 1,
        response_id: 6,
        request_id: 5,
        push_time: 1480563553177 },
      BinaryRow {
        userid: 1,
        response_id: 7,
        request_id: 5,
        push_time: 1480563553177 },
      BinaryRow {
        userid: 1,
        response_id: 8,
        request_id: 5,
        push_time: 1480563553177 } ]
    { pushing:
       { alias: '1',
         message: '{"userid":1,"response_id":6,"request_id":5,"push_time":1480563553177}' } }
    { pushing:
       { alias: '1',
         message: '{"userid":1,"response_id":7,"request_id":5,"push_time":1480563553177}' } }
    { pushing:
       { alias: '1',
         message: '{"userid":1,"response_id":8,"request_id":5,"push_time":1480563553177}' } }
    { 'post to PushNotiPlat':
       { StatusCode: 200,
         headers:
          { server: 'openresty/1.9.3.1',
            date: 'Thu, 01 Dec 2016 06:47:00 GMT',
            'content-type': 'application/json',
            'content-length': '75',
            connection: 'close' } } }
    {"status":5,"alias":"5821e7cf506122b4596f2b9d-1","error":"alias not found"}
    { 'post to PushNotiPlat':
       { StatusCode: 200,
         headers:
          { server: 'openresty/1.9.3.1',
            date: 'Thu, 01 Dec 2016 06:47:00 GMT',
            'content-type': 'application/json',
            'content-length': '75',
            connection: 'close' } } }
    {"status":5,"alias":"5821e7cf506122b4596f2b9d-1","error":"alias not found"}
    { 'post to PushNotiPlat':
       { StatusCode: 200,
         headers:
          { server: 'openresty/1.9.3.1',
            date: 'Thu, 01 Dec 2016 06:47:00 GMT',
            'content-type': 'application/json',
            'content-length': '75',
            connection: 'close' } } }
    {"status":5,"alias":"5821e7cf506122b4596f2b9d-1","error":"alias not found"}
    { name: 'scheduleScanner:',
      time: 2016-12-01T06:48:00.031Z,
      'currentTime(ms)': 1480574880031 }
    []
    ```

---
+++ pushScanner.js
@@ -0,0 +1,105 @@
+var mysql = require('mysql2/promise');
+var mysqlconf = require('./.conf.json').mysql;
+var schedule = require('node-schedule');
+var http = require('http');
+
+var appkey = '5821e7cf506122b4596f2b9d';
+var seckey = 'sec-JmBF4FqQa4AxF6cQ42g3qUCL1bHlZiLJHMMNM2f6Q8uRWnPl';
+
+function postToPushNotiPlat(alias, message){
+  var postData = JSON.stringify({
+    'method': 'publish_to_alias',
+    'appkey': appkey,
+    'seckey': seckey,
+    'alias': alias,
+    'msg': message,
+    'opts': {
+      'time_to_live': '604800',
+      'qos': '1'
+    }
+  });
+  var options = {
+    host: 'rest.yunba.io',
+    port: '8080',
+    path: '/',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(postData),
+    },
+  };
+
+  callback = function (response) {
+    console.log({'post to PushNotiPlat': {
+      'StatusCode': response.statusCode,
+      'headers': response.headers,
+    }});
+    var str = ''
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+  
+    response.on('end', function () {
+      console.log(str);
+    });
+
+  }
+
+  var postReq = http.request(options, callback)
+  .on('error', function(e) {
+    console.log(e);
+  });
+
+  postReq.write(postData);
+  postReq.end();
+}
+
+async function scheduleScanner() {
+    schedule.scheduleJob('0 * * * * *', async () => {
+        var currentTime = Date.now();
+        var timeInterval = 60000; 
+        console.log({
+          'name': 'scheduleScanner:',
+          'time': new Date(),
+          'currentTime(ms)': currentTime
+        });
+        let conn = await mysql.createConnection(mysqlconf);
+        let [rows, fields] = await conn.execute(
+          ` SELECT 
+              req.publisher_id AS userid,
+              res.response_id,
+              res.request_id,
+              res.push_time 
+            FROM available_responses res 
+            JOIN available_requests req
+            ON res.request_id = req.request_id
+            WHERE 
+              res.push_time >= ?
+              AND res.push_time <= ?
+              AND res.status IS NULL
+            ORDER BY push_time;
+          `,
+          [currentTime, currentTime + timeInterval * 2]
+        );
+        console.log(rows);
+        for (var i=0; i<rows.length; i++) {
+          var alias = rows[i].userid.toString();
+          var message = JSON.stringify(rows[i]);
+          console.log({"pushing": {"alias": alias, "message": message}});
+          postToPushNotiPlat(alias, message);
+        }
+        await conn.execute(
+          ` UPDATE available_responses 
+            SET status='pushed'
+            WHERE 
+              push_time >= ?
+              AND push_time <= ?
+              AND status IS NULL;
+          `,
+          [currentTime, currentTime + timeInterval * 2]
+        );
+    }); 
+}
+
+scheduleScanner();
+
